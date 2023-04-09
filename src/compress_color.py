43fb7eaf8e85bb9990fab60bc273293745302bbd
EtomicBomb <ethan@ethan.ws>
2023-04-08 21:14:40 -0400

    working

--- src/compress_color.py
+++ src/compress_color.py
@@ -27,41 +27,28 @@ def snake(size):
         reset = np.flip(reset)
         dxdy = np.flip(dxdy)
 
-def encode_channel(channel, chunk_size, snake_r, snake_c):
-    channel_height, channel_width = channel.shape
-    assert channel_height % chunk_size == 0
-    assert channel_width % chunk_size == 0
-    channel = view_as_blocks(channel, (chunk_size, chunk_size))
-    channel = dctn(channel, axes=(2,3), norm='ortho')
-    channel = channel[:, :, snake_r, snake_c]
-    return channel
-
 def encode(image, chunk_size, snake_len):
     ycbcr = rgb2ycbcr(image)
-    y, cb, cr = ycbcr[:,:,0], ycbcr[:,:,1], ycbcr[:,:,2]
+    y, color_prime = ycbcr[:,:,0], ycbcr[:,:,1:]
 
     snake_r, snake_c = snake(snake_len)
-    cb_prime = encode_channel(cb, chunk_size, snake_r, snake_c)
-    cr_prime = encode_channel(cr, chunk_size, snake_r, snake_c)
 
-    return y, cb_prime, cr_prime
+    color_prime = view_as_blocks(color_prime, (chunk_size, chunk_size, 1))
+    color_prime = dctn(color_prime, axes=(3,4), norm='ortho')
+    color_prime = color_prime[:, :, :, snake_r, snake_c, :]
 
-def decode_channel(encoded, chunk_size, snake_r, snake_c):
+    return y, color_prime
+
+def decode(y, color_prime, chunk_size, snake_len):
+    snake_r, snake_c = snake(snake_len)
     channel_height_chunks, channel_width_chunks, _ = encoded.shape
-    channel = np.zeros((channel_height_chunks, channel_width_chunks, chunk_size, chunk_size))
-    channel[:, :, snake_r, snake_c] = encoded[:, :, :]
-    channel = idctn(channel, axes=(2,3), norm='ortho')
-    channel = np.moveaxis(channel, (0, 2, 1, 3), (0, 1, 2, 3))
-    channel = np.reshape(channel, (channel_height_chunks*chunk_size, channel_width_chunks*chunk_size))
-    return channel
+    color = np.zeros((channel_height_chunks, channel_width_chunks, chunk_size, chunk_size))
+    color[:, :, :, snake_r, snake_c, :] = color_prime
+    color = idctn(color, axes=(3,4), norm='ortho')
+    channel = np.swapaxis(channel, 1, 2)
+    color = np.reshape(color, (channel_height_chunks*chunk_size, channel_width_chunks*chunk_size, 2))
 
-def decode(y, cb_prime, cr_prime, chunk_size, snake_len):
-    snake_r, snake_c = snake(snake_len)
-    cb = decode_channel(cb_prime, chunk_size, snake_r, snake_c)
-    cr = decode_channel(cr_prime, chunk_size, snake_r, snake_c)
-
-    ycbcr = np.stack((y, cb, cr), axis=-1)
-    return ycbcr2rgb(ycbcr)
+    return ycbcr2rgb(np.dstack((y, color)))
 
 def encode_whole(image, snake_len):
     ycbcr = rgb2ycbcr(image)
@@ -90,10 +77,10 @@ image = img_as_float(imread('square4.png'))
 h, w, _ = image.shape
 image = resize(image, (h-h%chunk_size, w-w%chunk_size, 3))
 
-y, color_prime = encode_whole(image, snake_len)
-image2 = decode_whole(y, color_prime, snake_len)
-#y, cb_prime, cr_prime = encode(image, chunk_size, snake_len)
-#image2 = decode(y, cb_prime, cr_prime, chunk_size, snake_len)
+#y, color_prime = encode_whole(image, snake_len)
+#image2 = decode_whole(y, color_prime, snake_len)
+y, color_prime = encode(image, chunk_size, snake_len)
+image2 = decode(y, color_prime, chunk_size, snake_len)
 
 print('old color parameters', image[:,:,:2].size)
 print('new color parameters', color_prime.size)
