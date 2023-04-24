57cf06001976e35d2be96449082a5a95c8ede3d2
nbenabla <77747926+nbenabla@users.noreply.github.com>
2023-04-23 23:50:17 -0400

    add initial UI layout

---
+++ src/ui/ui.js
@@ -0,0 +1,75 @@
+
+// Get the input image and canvas elements
+const upload = document.getElementById('upload');
+upload.onclick = uploadImage();
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+const actualBtn = document.getElementById('upload');
+
+const fileChosen = document.getElementById('file-chosen');
+
+const clear = document.getElementById('clear');
+
+
+const selectedColor = document.getElementById('selectedColor');
+
+const outputImage = document.getElementById('outputImage');
+
+function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
+
+    var ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
+
+    return { width: srcWidth*ratio, height: srcHeight*ratio };
+ }
+
+// When an image is selected, display it on the canvas
+function uploadImage() {
+upload.addEventListener('change', function() {
+  const file = upload.files[0];
+  const reader = new FileReader();
+  reader.onload = function(event) {
+    const image = new Image();
+    image.onload = function() {
+      canvas.width = 400;
+      canvas.height = 350;
+
+    var ratio = calculateAspectRatioFit(image.width, image.height, canvas.width, canvas.height);
+    var xOffset = ratio.width < canvas.width ? ((canvas.width - ratio.width) / 2) : 0;
+    var yOffset = ratio.height < canvas.height ? ((canvas.height - ratio.height) / 2) : 0;
+
+      context.drawImage(image, xOffset, yOffset, ratio.width, ratio.height);
+      colorPalette.style.visibility="visible";
+      clear.style.visibility="visible";
+    };
+    image.src = event.target.result;
+
+  };
+  reader.readAsDataURL(file);
+
+});
+}
+
+// Draw on the canvas using the selected color
+function draw() {
+  var color = getSelectedColor();
+  canvas.addEventListener('mousedown', function(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    context.fillStyle = color;
+    context.fillRect(x, y, 5, 5);
+  });
+}
+
+// Get the selected color from the color palette
+function getSelectedColor() {
+    return selectedColor.value;
+}
+
+
+// Clear the canvas
+function clearCanvas() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  colorPalette.style.visibility="hidden";
+  clear.style.visibility="hidden";
+}
