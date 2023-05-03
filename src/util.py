2e7bbd3d936be6c27163dfad1034eb354b778185
EtomicBomb <ethan@ethan.ws>
2023-05-03 13:42:40 -0400

    after training new model

--- src/util.py
+++ src/util.py
@@ -35,25 +35,40 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
 
     # input can come from numpy, memory, anywhere
-    image = imread('flower.jpg')
+#    image = imread('flower.jpg')
+#    image = imread('raw/40410686_272bc66faf_m.jpg')
+#    image = imread('raw/130684941_d1abfa3be6_m.jpg')
+#    image = imread('raw/2767658405_1e2043f44c_n.jpg')
+    image = imread('oxford/test/image_00658.jpg') # does a pretty poor job at this one
+#    image = imread('oxford/test/image_04027.jpg')
+    image = imread('oxford/test/image_08089.jpg')
+    image = imread('oxford/test/image_08033.jpg')
+#    image = imread('oxford/test/image_07937.jpg')  # nails this one with no hints!
+#    image = imread('oxford/test/image_08186.jpg') # you can hint the color of this flower! [70,70]
+#    image = imread('oxford/train/image_03929.jpg')
     image = img_as_float32(image)
     image = resize(image, (image_height, image_width, 3), anti_aliasing=True)
 
     points = [
 #        [20,20],
-#        [50,50],
+        [50,50],
+        [90,90],
 #        [223,210],
-#        [223,223],
+#        [50,112],
+#        [175,175],
     ]
 
     colors = [
 #        [255, 0, 0],
-#        [0, 255, 0],
+        [0, 255, 0],
 #        [0, 0, 255],
-#        [255, 255, 0],
+#        [0, 255, 255],
+        [255, 128, 0],
     ]
 
-    model = tf.keras.models.load_model('check/d.h5')
+    model = tf.keras.models.load_model('check/f.h5')
+#    model = tf.keras.models.load_model('check/e.h5')
+#    model = tf.keras.models.load_model('check/d.h5')
 
     # convert before giving to colorize
 
@@ -70,9 +85,13 @@ if __name__ == '__main__':
 
     # predict using generated points
     image = image[:,:,:1]
+    points = tf.reshape(points, (-1, 2))
     points = tf.cast(points, tf.dtypes.float32)
+    colors = tf.reshape(colors, (-1, 3))
     colors = tf.cast(colors, tf.dtypes.float32)
-    colors = tf.image.rgb_to_yuv(colors / 255.0)
+    print(colors.shape)
+    if colors.shape[0] != 0:
+        colors = tf.image.rgb_to_yuv(colors / 255.0)
     colors = colors[:,1:]
     predicted = colorize(image, points, colors, model)
     predicted = tf.image.yuv_to_rgb(predicted)
